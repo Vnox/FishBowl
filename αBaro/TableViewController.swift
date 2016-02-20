51b5b65cf2b34e74b6c227ba9cc77665c331d4bf
GMaple <ethereopc1@ethereos-mbp.attlocal.net>
2016-02-19 21:38:19 -0800

    Fix the core data store problems

--- αBaro/TableViewController.swift
+++ αBaro/TableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController, TableViewCellDelegate {
     
@@ -14,9 +15,11 @@ class TableViewController: UITableViewController, TableViewCellDelegate {
     
 
     var events = [Event]()
+    var eventData = [NSManagedObject]()
     let nonurgentImg = UIImage(named: "urgentIndiGr")
     let urgentImg = UIImage(named: "urgentIndi")
     var detailRow = [Int]()
+    var managedContext: NSManagedObjectContext!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,11 +53,28 @@ class TableViewController: UITableViewController, TableViewCellDelegate {
     
     func loadEvents() {
         
-        let event1:Event = Event(name: "Finish math Homwwork", timeRemaining: 4.00, priority: false)
-        let event2:Event = Event(name: "CSE Test Review", timeRemaining: 2.00, priority: false)
-        let event3:Event = Event(name: "Getting ready for the driving test", timeRemaining: 1.00, priority: true)
-        events += [event1, event2, event3]
+//        let event1:Event = Event(name: "Finish math Homwwork", timeRemaining: 4.00, priority: false)
+//        let event2:Event = Event(name: "CSE Test Review", timeRemaining: 2.00, priority: false)
+//        let event3:Event = Event(name: "Getting ready for the driving test", timeRemaining: 1.00, priority: true)
+//        events += [event1, event2, event3]
         
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        managedContext = appDelegate.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: "Entity")
+        
+        do{
+            let results =
+            try managedContext!.executeFetchRequest(fetchRequest)
+            eventData = results as! [NSManagedObject]
+            for var i=0; i<eventData.count; i++ {
+                let tmpEvent = eventData[i]
+                let eventObj: Event = Event(name: (tmpEvent.valueForKey("name") as? String)!, timeRemaining: 4.00, priority: false)
+                events.append(eventObj)
+            }
+            print("\(eventData)")
+        } catch let error as NSError {
+            print("could not fetch \(error), \(error.userInfo)")
+        }
         
     }
     
